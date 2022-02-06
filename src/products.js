041abde9c643f8d94c335ccdc3a695c5c32d4fea
Gourav Goel <47769737+2701gouravgoel@users.noreply.github.com>
2022-02-06 19:36:48 +0530

    connect with server

--- src/products.js
+++ src/products.js
@@ -1,34 +1,18 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './App.css';
 import style from './products.css';
 const Products = () => {
-  const products=[
-    {
-      id:1,
-      name:'Basmati Rice 1',
-      imageUrl:'https://picsum.photos/200',
-      price:400
-    },
-    {
-      id:2,
-      name:'Basmati Rice 2',
-      imageUrl:'https://picsum.photos/201',
-      price:400
-    },
-    {
-      id:3,
-      name:'Basmati Rice 3',
-      imageUrl:'https://picsum.photos/202',
-      price:400
-    },
-    {
-      id:4,
-      name:'Basmati Rice 4',
-      imageUrl:'https://picsum.photos/203',
-      price:400
-    }
-  ]
-
+  let [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("https://double-chabi-server.herokuapp.com/getProducts")
+    .then((res) => res.json())
+    .then((json) => {
+        setProducts(json.list);
+    })
+  }, [])
+  console.log(products);
+  
     const elm = products.map((i, index) => {
       return  <Link to={`/Product?id=${i.id}`}>
       <div className="msgCenter" key={index}>
