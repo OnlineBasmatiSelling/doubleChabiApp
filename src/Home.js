13c9e6d1fcab1d05770d0b727789a7bd64e70ddc
Gourav Goel <47769737+2701gouravgoel@users.noreply.github.com>
2022-02-07 12:11:13 +0530

    added home page

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,44 @@
-import './App.css';
+import { useState } from 'react';
+import { Carousel } from 'react-bootstrap';
+import './home.css';
+import home1 from './home1.jpg';
+import home2 from './home2.jpg';
 const Home = () => {
-    return <h1>Home</h1>;
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex, e) => {
+      setIndex(selectedIndex);
+    };
+    return <div><Carousel fade slide activeIndex={index} onSelect={handleSelect} className='carousel container-fluid'>
+    <Carousel.Item>
+      <img
+        src={home1}
+        alt="First slide"
+      />
+      {/* <Carousel.Caption>
+        <h3>First slide label</h3>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      </Carousel.Caption> */}
+    </Carousel.Item>
+    <Carousel.Item>
+      <img
+        src={home2}
+        alt="Second slide"
+      />
+  
+      {/* <Carousel.Caption>
+        <h3>Second slide label</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </Carousel.Caption> */}
+    </Carousel.Item>
+    {/* <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src={home2}
+        alt="Third slide"
+      />
+    </Carousel.Item> */}
+  </Carousel></div>
   };
 
 export default Home;
