bdf6d2f15cd6c5d9e12ebf7cd873c42d1592f277
Gourav Goel <47769737+2701gouravgoel@users.noreply.github.com>
2022-02-15 23:26:55 +0530

    Update FirebaseConfig.js

--- src/configuration/FirebaseConfig.js
+++ src/configuration/FirebaseConfig.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 const firebaseConfig = {
-  apiKey: "AIzaSyCSU6oXmWTAgjt5jwFUdo27OkF75G7z4Wk",
-  authDomain: "doublechabiapp.firebaseapp.com",
-  projectId: "doublechabiapp",
-  storageBucket: "doublechabiapp.appspot.com",
-  messagingSenderId: "40469898061",
-  appId: "1:40469898061:web:3a63c45d1835142e12cd04",
-  measurementId: "G-YGL96LH6VD"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 let secondaryAuth = {};
 
